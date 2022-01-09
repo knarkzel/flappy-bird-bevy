fd63322c0cefe0dbbdb538c4c72314402b600bcc
Knarkzel <knarkzel@gmail.com>
2022-01-09 02:20:46 +0100

    docs

--- wasm.sh
+++ wasm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir public --target web target/wasm32-unknown-unknown/release/flappy-bird.wasm
-wasm-opt -Oz -o public/flappy-bird_bg.wasm public/flappy-bird_bg.wasm
-ls -alh public/flappy-bird_bg.wasm 
+wasm-bindgen --out-dir docs --target web target/wasm32-unknown-unknown/release/flappy-bird.wasm
+wasm-opt -Oz -o docs/flappy-bird_bg.wasm docs/flappy-bird_bg.wasm
+ls -alh docs/flappy-bird_bg.wasm 
